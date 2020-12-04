85e8298788e90d9cafb9f0b56f7f68efc516dd50
Barkha Tharani <barkhatharani@Barkhas-MacBook-Air.local>
2020-12-05 01:13:38 +0530

    Task 1 - Addition upto two numbers.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CommandLineCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0) return;
+
+            var operationType = args[0];
+
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Please enter valid number of arguments separated by ','");
+                return;
+            }
+
+            string nums = args.Length == 2 ? args[1] : String.Empty;
+
+            string[] strings = !String.IsNullOrEmpty(nums) ? nums.Split(',') : new string[0];
+            int[] list = Array.ConvertAll(strings, s => int.Parse(s));
+            long result = 0;
+
+            switch (operationType)
+            {
+                case "sum":
+                    result = AddTwoNumbers(list);
+                    Console.WriteLine(result);
+                    break;
+
+                default:
+                    break;
+            }
+            
+        }
+
+        static long AddTwoNumbers(params int[] list)
+        {
+            if (list.Length <= 0)
+                return 0;
+
+            if (list.Length > 2)
+                throw new NotSupportedException("Addition upto two numbers is supported");
+
+            long sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum = sum + list[i];
+            }
+
+            return sum;
+
+        }
+    }
+}
