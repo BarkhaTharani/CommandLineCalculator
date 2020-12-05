1b580d78d7165a78449998ad63e1b1ac756e8c34
Barkha Tharani <barkhatharani@Barkhas-MacBook-Air.local>
2020-12-05 12:43:57 +0530

    Task 8 - Making multiply functionality.

--- Program.cs
+++ Program.cs
@@ -18,29 +18,29 @@ namespace CommandLineCalculator
                 throw new ArgumentException(@"Please enter valid number of arguments.");
             }
 
-           string nums = args.Length == 2 ? args[1] : String.Empty;
-           string[] inputString = new string[0];
-           string delimeterChar = String.Empty;
-           string inputNumbers = String.Empty;
+            string nums = args.Length == 2 ? args[1] : String.Empty;
+            string[] inputString = new string[0];
+            string delimeterChar = String.Empty;
+            string inputNumbers = String.Empty;
 
-           if(!nums.StartsWith("\\"))
-           {
+            if(!nums.StartsWith("\\"))
+            {
                 throw new FormatException(errorMsg);
-           }
+            }
 
             inputString = nums.Split("\\");
 
-            if(inputString.Length > 3) 
+            if(inputString.Length > 3)
             {
                 throw new FormatException(errorMsg);
             }
 
             delimeterChar = inputString[1].Trim();
             inputNumbers = inputString[2] ;
-           
+
 
             string[] strings = !String.IsNullOrEmpty(inputNumbers) ? inputNumbers.Split(delimeterChar, StringSplitOptions.None ) : new string[0];
-            
+
             int[] list = Array.ConvertAll(strings, s => int.Parse(s));
 
             var numbersLessThanThousand = list.Where( i => i <= 1000).ToArray();
@@ -62,10 +62,16 @@ namespace CommandLineCalculator
                     Console.WriteLine(result);
                     break;
 
+                case "multiply":
+                    result = Multiply(numbersLessThanThousand);
+                    Console.WriteLine(result);
+                    break;
+
                 default:
+                    Console.WriteLine(String.Format("Operation type {0} not supported", operationType));
                     break;
             }
-            
+
         }
 
         static long Add(params int[] list)
@@ -80,6 +86,21 @@ namespace CommandLineCalculator
             }
 
             return sum;
+
+        }
+
+        static long Multiply(params int[] list)
+        {
+            if (list.Length <= 0)
+                return 0;
+
+            long result = 1;
+            for (int i = 0; i < list.Length; i++)
+            {
+                result = result * list[i];
+            }
+
+            return result;
 
         }
     }
