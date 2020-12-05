38aec30e1c13e2dddf056beeb995ed38070b0464
Barkha Tharani <barkhatharani@Barkhas-MacBook-Air.local>
2020-12-05 11:48:59 +0530

    Task 4 - Support different delimeters.

--- Program.cs
+++ Program.cs
@@ -10,24 +10,42 @@ namespace CommandLineCalculator
 
             var operationType = args[0];
 
+            string errorMsg = @"Please provide input in the format \\[delimiter]\\numbers";
+
             if (args.Length > 2)
             {
-                Console.WriteLine("Please enter valid number of arguments separated by ','");
-                return;
+                throw new ArgumentException(@"Please enter valid number of arguments.");
             }
 
            string nums = args.Length == 2 ? args[1] : String.Empty;
+           string[] inputString = new string[0];
+           string delimeterChar = String.Empty;
+           string inputNumbers = String.Empty;
 
-            string[] splitChar = {"," , "\\n"};
+           if(!nums.StartsWith("\\"))
+           {
+                throw new FormatException(errorMsg);
+           }
 
-            string[] strings = !String.IsNullOrEmpty(nums) ? nums.Split(splitChar, StringSplitOptions.None ) : new string[0];
+            inputString = nums.Split("\\");
+
+            if(inputString.Length > 3) 
+            {
+                throw new FormatException(errorMsg);
+            }
+
+            delimeterChar = inputString[1].Trim();
+            inputNumbers = inputString[2] ;
+           
+
+            string[] strings = !String.IsNullOrEmpty(inputNumbers) ? inputNumbers.Split(delimeterChar, StringSplitOptions.None ) : new string[0];
             
             int[] list = Array.ConvertAll(strings, s => int.Parse(s));
             long result = 0;
 
             switch (operationType)
             {
-                case "sum":
+                case "add":
                     result = Add(list);
                     Console.WriteLine(result);
                     break;
